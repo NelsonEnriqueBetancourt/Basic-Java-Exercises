ca6dfb2dd2c98d6d68b2b8063fd581c21a617b78
nelson.betancourt <nelson.betancourt@globant>
2022-01-17 16:36:01 -0500

    First Commit in the repository

---
+++ src/Exercise1/CheckPassFail.java
@@ -0,0 +1,17 @@
+package Exercise1;
+
+public class CheckPassFail {
+    public static void main(String[] args) {
+        int mark = 50;
+        System.out.println("the mark is " + mark );
+
+        // Conditional if - else
+        if (mark >= 50 )  {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
+    }
+
+
+}
---
+++ src/Exercise2/Hello.java
@@ -0,0 +1,7 @@
+package Exercise2;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello\nNelson Betancourt Lopez");
+    }
+}
