b14a5cb5b21f953585f2fe738581b4723b8f284b
nelson.betancourt <nelson.betancourt@globant>
2022-02-11 00:02:52 -0500

    New practice Exercises, week 3 !!

--- src/Challengetopic1/Exercise1/Factory/Main.java
+++ src/Challengetopic1/Exercise1/Factory/Main.java
@@ -1,8 +1,12 @@
 package Challengetopic1.Exercise1.Factory;
 
+import Challengetopic1.Exercise1.Clases.Reproductor1;
+
 
 public class Main {
     public static void main(String[] args) {
+        Reproductor1 R1 = new Reproductor1();
+        R1.Personalizar1();
 
     }
 }
